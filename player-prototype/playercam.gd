ddfb55e8d7a6b4f93355614083f6beaf7d8d9aae
mozes-lol <63577510+mozes-lol@users.noreply.github.com>
2022-12-14 14:29:18 +0800

    Implement Camera Smoothing

--- player-prototype/playercam.gd
+++ player-prototype/playercam.gd
@@ -1,14 +1,14 @@
 extends Node2D
 
 onready var player = get_node("/root/leveldemo/player_A")
-
+var followSpeed = 0.35 # Min = 0; Max = 1
 
 func _ready():
 	pass
 
 func _process(_delta):
 	var mouse = get_viewport().get_mouse_position()
-	position = player.position
+	position = lerp(position, player.position, followSpeed)
 
 	
 ##	if mouse != Vector2(0, 0):
