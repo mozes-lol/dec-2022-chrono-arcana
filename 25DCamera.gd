0fd70c9d0c96d81d25e634c8118f1e788ce3f963
mozes-lol <johnmarkgarcia164@gmail.com>
2022-12-14 23:10:06 +0800

    Implement Camera Smoothing

--- 25DCamera.gd
+++ 25DCamera.gd
@@ -5,6 +5,7 @@ var cameraPosition
 var cameraOffsetPositionToPlayer = Vector3.ZERO
 var playerTargetPositionPerFrame = Vector3.ZERO
 export var cameraManualOffset = Vector3.ZERO # If further offset tweaking is not needed, leave the values for all axis as 0
+export var followSpeed = 0.1 # How fast does the camera follow the player
 
 func _ready():
 	
@@ -22,13 +23,9 @@ func _ready():
 	
 	# This is the final offset that will be used
 	cameraOffsetPositionToPlayer = Vector3(offsetX, offsetY, offsetZ) + cameraManualOffset
-	print_debug(cameraOffsetPositionToPlayer)
 
 func _process(delta: float) -> void:
+	Engine.set_target_fps(Engine.get_iterations_per_second()) # idk if this will solve smoothing jitter... UPDATE: It did lol
 	# The position of the player will be checked every frame
 	playerTargetPositionPerFrame = get_node("/root/Map_Prototype/25DPlayerTest").translation
-	translation = playerTargetPositionPerFrame + cameraOffsetPositionToPlayer
-	print_debug(playerTargetPositionPerFrame)
-	
-func _physics_process(delta: float) -> void:
-	translation = playerTargetPositionPerFrame + cameraOffsetPositionToPlayer
+	translation = lerp(translation, playerTargetPositionPerFrame + cameraOffsetPositionToPlayer, followSpeed)
