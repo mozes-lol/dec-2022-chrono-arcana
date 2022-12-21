e2dddf01aa46446ec87ab77d1e8099179b1ea9c4
Zekiru <zekiel.villasurda@gmail.com>
2022-12-21 18:27:33 +0800

    Multiple Polishing

--- AnimatedSprite3D.gd
+++ AnimatedSprite3D.gd
@@ -2,15 +2,22 @@ extends AnimatedSprite3D
 
 export var normal_animspeed = 10
 
-var dashing = false
-var running = false
+var dashing
+var running
 
 onready var move_keys = get_parent().move_keys
 
 func _process(_delta):
 	move_keys = get_parent().move_keys
-	dashing = get_parent().dashing
-	running = get_parent().running
+	
+	dashing = false
+	running = false
+		
+	if move_keys[5]:
+		dashing = true
+	
+	if move_keys.has(true):
+		running = true
 	
 	play()
 	frames.set_animation_speed("idle", normal_animspeed)
@@ -18,6 +25,6 @@ func _process(_delta):
 	if running:
 		frames.set_animation_speed("walk", normal_animspeed)
 	
-	if dashing:
+	if dashing || !get_parent().is_on_floor():
 		frame = 1
 		stop()
