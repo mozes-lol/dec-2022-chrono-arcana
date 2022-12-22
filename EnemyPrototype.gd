341f0c0836b62aebacdc8753e1c663b59de7a842
mozes-lol <johnmarkgarcia164@gmail.com>
2022-12-22 19:58:52 +0800

    Enemy Face on Move Direction

--- EnemyPrototype.gd
+++ EnemyPrototype.gd
@@ -2,9 +2,11 @@ extends KinematicBody
 
 var path = []
 var path_node = 0
-var is_attacking = false
+var is_preparing_to_attack = false
+var direction = Vector3.ZERO
 
 var speed = 10 # how fast the enemy moves to the player
+var dash_attack_speed = 50
 
 func _ready() -> void:
 	$Timer.start()
@@ -12,11 +14,20 @@ func _ready() -> void:
 onready var nav = get_parent() # gets the NavigationMeshInstance Node
 onready var player = $"/root/Map_Prototype/Navigation/25DPlayerTest"
 
+func _process(delta: float) -> void:
+	print_debug(direction)
+	# We flip the sprite depending on the direction of this enemy.
+	# (Yes, we'll prolly use the animated sprite node later on; But this is just for demonstration purposes.)
+	if direction.x > 0:
+		$Sprite3D.flip_v = false
+	elif direction.x < 0:
+		$Sprite3D.flip_v = true
+
 func _physics_process(delta: float) -> void:
-	if is_attacking == true:
+	if is_preparing_to_attack == true:
 		return # We do a guard clause to prevent the enemy from doing anymore movement in the time being.
 	if path_node < path.size():
-		var direction = (path[path_node] - global_transform.origin)
+		direction = (path[path_node] - global_transform.origin)
 		if direction.length() < 1:
 			path_node += 1
 		else:
@@ -30,4 +41,4 @@ func _on_Timer_timeout() -> void:
 	move_to(player.global_transform.origin)
 
 func _on_Area_body_entered(body: Node) -> void:
-	is_attacking = true
+	is_preparing_to_attack = true
