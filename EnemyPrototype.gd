8ffcb64d43969f705b7fcbd5ade7a37b8a3ac8b9
mozes-lol <johnmarkgarcia164@gmail.com>
2023-01-05 15:02:35 +0800

    Implement Enemy Attack Pounce

--- EnemyPrototype.gd
+++ EnemyPrototype.gd
@@ -1,13 +1,21 @@
 extends KinematicBody
 
+# Enemy Movement
 export var path_to_player := NodePath()
 export var move_speed = 5
-
 var _velocity := Vector3.ZERO
-
 onready var _agent: NavigationAgent = $NavigationAgent
 onready var _player = get_node(path_to_player)
-onready var _timer: Timer = $Timer
+onready var _timer: Timer = $Pathfinding
+
+#Enemy Attack
+onready var attackPrepare: Timer = $AttackPrepare
+onready var attackDuration: Timer = $AttackDuration
+onready var attackCooldown: Timer = $AttackCooldown
+onready var attackCollision: CollisionShape = $AttackRange/CollisionShape
+var can_attack = true
+var is_preparing_to_attack = false
+var is_attacking = false
 
 func _ready() -> void:
 	_agent.set_target_location(_player.global_translation)
@@ -15,6 +23,12 @@ func _ready() -> void:
 	
 func _physics_process(delta: float) -> void:
 	global_translation.y = 1
+		
+	if is_preparing_to_attack == true:
+		return
+	elif is_attacking == true:
+		move_and_slide(_velocity)
+		return
 	
 	if _agent.is_navigation_finished():
 		return
@@ -29,3 +43,23 @@ func _physics_process(delta: float) -> void:
 
 func _update_pathfinding() -> void:
 	_agent.set_target_location(_player.global_translation)
+
+func _on_AttackRange_body_entered(body: Node) -> void:
+	if can_attack == true:
+		attackCollision.disabled = true
+		attackPrepare.start()
+		is_preparing_to_attack = true
+		can_attack = false
+
+func _on_AttackPrepare_timeout() -> void:
+	attackDuration.start()
+	is_preparing_to_attack = false
+	is_attacking = true
+
+func _on_AttackDuration_timeout() -> void:
+	attackCooldown.start()
+	is_attacking = false
+
+func _on_AttackCooldown_timeout() -> void:
+	can_attack = true
+	attackCollision.disabled = false
