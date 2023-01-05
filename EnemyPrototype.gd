af89b0748f36e0285d2a7c62cf03d47d8d4b3fd8
mozes-lol <johnmarkgarcia164@gmail.com>
2023-01-05 13:19:35 +0800

    Rework Enemy Pathfinding
    I've utilized the new pathfinding feature from Godot 3.5. Now, the movement is buttery smooth

--- EnemyPrototype.gd
+++ EnemyPrototype.gd
@@ -1,45 +1,31 @@
 extends KinematicBody
 
-var path = []
-var path_node = 0
-var is_preparing_to_attack = false
-var direction = Vector3.ZERO
+export var path_to_player := NodePath()
+export var move_speed = 5
 
-var speed = 10 # how fast the enemy moves to the player
-var dash_attack_speed = 50
+var _velocity := Vector3.ZERO
+
+onready var _agent: NavigationAgent = $NavigationAgent
+onready var _player = get_node(path_to_player)
+onready var _timer: Timer = $Timer
 
 func _ready() -> void:
-	$Timer.start()
-
-onready var nav = get_parent() # gets the NavigationMeshInstance Node
-onready var player = $"/root/Map_Prototype/Navigation/25DPlayerTest"
-
-func _process(delta: float) -> void:
-	print_debug(direction)
-	# We flip the sprite depending on the direction of this enemy.
-	# (Yes, we'll prolly use the animated sprite node later on; But this is just for demonstration purposes.)
-	if direction.x > 0:
-		$Sprite3D.flip_v = false
-	elif direction.x < 0:
-		$Sprite3D.flip_v = true
-
+	_agent.set_target_location(_player.global_translation)
+	_timer.connect("timeout", self, "_update_pathfinding")
+	
 func _physics_process(delta: float) -> void:
-	if is_preparing_to_attack == true:
-		return # We do a guard clause to prevent the enemy from doing anymore movement in the time being.
-	if path_node < path.size():
-		direction = (path[path_node] - global_transform.origin) 
-		direction.y += .45  # This will push the enemy upward, preventing it from "semi-sinking" to the ground
-		if direction.length() < 1:
-			path_node += 1
-		else:
-			move_and_slide(direction.normalized()  * speed, Vector3.UP)
+	global_translation.y = 1
+	
+	if _agent.is_navigation_finished():
+		return
+	
+	var direction = global_translation.direction_to(_agent.get_next_location())
+	
+	var desired_velocity = direction * move_speed
+	var steering = (desired_velocity - _velocity) * delta * 4.0
+	_velocity += steering
+	
+	_velocity = move_and_slide(_velocity)
 
-func move_to(target_pos):
-	path = nav.get_simple_path(global_transform.origin, target_pos)
-	path_node = 0
-
-func _on_Timer_timeout() -> void:
-	move_to(player.global_transform.origin)
-
-func _on_Area_body_entered(body: Node) -> void:
-	is_preparing_to_attack = true
+func _update_pathfinding() -> void:
+	_agent.set_target_location(_player.global_translation)
